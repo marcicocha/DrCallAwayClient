c707545ee7282d6aacd16e4c3f3b182c340d6086
unknown <marcia.cole@longbridgetech.com>
2021-10-19 19:37:36 +0100

    fix: update on prescription

--- components/admin/patient/precription-test/prescription/AppPrescriptionForm.vue
+++ components/admin/patient/precription-test/prescription/AppPrescriptionForm.vue
@@ -66,7 +66,7 @@
     <br />
     <div v-if="prescriptionObj.status === 'PENDING'" class="t-c">
       <AppButton
-        v-if="prescriptionObj.partners_id"
+        v-if="prescriptionObj.partners.specialty_id"
         type="primary"
         :block="false"
         :loading="isLoading"

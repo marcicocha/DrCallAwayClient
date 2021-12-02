722a88750a38538b9585cd94eed38ca8f9763676
unknown <marcia.cole@longbridgetech.com>
2021-12-02 21:41:08 +0100

    fix: update on main

--- plugins/pusher.js
+++
@@ -1,8 +0,0 @@
-const Pusher = require('pusher')
-const pusher = new Pusher(process.env.PUSHER_APP_KEY, {
-  key: process.env.PUSHER_APP_KEY,
-  cluster: process.env.PUSHER_APP_CLUSTER,
-  secret: process.env.PUSHER_APP_SECRET,
-  app_id: process.env.PUSHER_APP_ID,
-})
-export default pusher
